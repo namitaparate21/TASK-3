3927bf2218841910f407b6c49f508db4916af899
namitaparate21 <namitaparate33@gmail.com>
2024-10-03 16:27:04 +0530

    Add files via upload
    TASK-3 completed

---
+++ ecom.query.sql.sql
@@ -0,0 +1,34 @@
+select * from sales
+
+select * from customer
+
+select * from product
+
+.....dim_customer.....
+
+create table dim_customer as
+select customer_id,customer_name,segment,age,postal_code
+from customer
+
+select * from dim_customer
+
+.....sub_dim_region......
+
+create table sub_dim_region as
+select distinct postal_code,city,state,region
+from customer
+
+select * from sub_dim_region
+
+......sub_dim_product_category.....
+
+alter table product add column category_id serial ;
+
+create table sub_dim_product_category as
+select category_id , category , sub_category from product
+
+
+select * from sub_dim_product_category
+
+
+
